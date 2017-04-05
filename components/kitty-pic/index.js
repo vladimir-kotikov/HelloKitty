0213b28ac20004b6e9ef916e8486678b8d5c2dfb
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2017-04-05 23:55:47 +0300

    Make it crashing

--- components/kitty-pic/index.js
+++ components/kitty-pic/index.js
@@ -5,24 +5,21 @@ import Sound from 'react-native-sound';
 
 export class KittyPic extends Component {
 
-    sound = null;
-    pic = require('./kitty.jpg');
-    mp3 = require('./meow.mp3');
+    counter = 0;
+    picture = require('./kitty.jpg');
+    sounds = [
+        new Sound(require('./meow1.mp3')),
+        new Sound(require('./meow2.mp3'))
+    ];
 
     meow() {
-        if (this.sound && this.sound.isLoaded) {
-            this.sound.play();
-        }
-    }
-
-    componentDidMount() {
-        this.sound = new Sound(this.mp3);
+        this.sounds[this.counter++].play();
     }
 
     render() {
         return (
             <View onTouchEnd={() => this.meow()}>
-                <Image source={this.pic}/>
+                <Image source={this.picture}/>
             </View>
         );
     }
